bb703b21e909e4cbd74bd5eef6799cc1a61bfad1
abhinav-kesari <60466018+abhinav-kesari@users.noreply.github.com>
2022-01-29 19:25:11 +0530

    Add files via upload
    chap_02

---
+++ 03_pr_avg.py
@@ -0,0 +1,3 @@
+a, b = int(input()),int(input())
+avg = (a+b)/2
+print(avg)
---
+++ typecasting.py
@@ -0,0 +1,11 @@
+a="Abhinav"
+print(len(a))
+
+b="23"
+print(b)
+print(type(b))
+
+
+print(int(b))
+c=int(b)
+print(type(c))
